b68339a52b29a3f0606276a32bb1a913ac129e4c
Iman Taouil <itaouil@c2r6p9.42nice.fr>
2022-05-30 17:17:03 +0200

    gestion de leaks toussa toussa

--- export.c
+++ export.c
@@ -6,14 +6,28 @@
 /*   By: itaouil <itaouil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 19:13:05 by itaouil           #+#    #+#             */
-/*   Updated: 2022/05/28 21:03:13 by itaouil          ###   ########.fr       */
+/*   Updated: 2022/05/30 13:51:50 by itaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	create_env_entry(t_list **env, char *var, char *value)
+{
+	t_env *entry;
+
+	entry = malloc(sizeof(t_env));
+	entry->variable = var;
+	entry->value = value;
+	ft_lstadd_back(env, ft_lstnew(entry));
+}
+
 void	ft_export(t_list **env, char *var)
 {
 	if (!var)
 		print_env_in_ascii_order(*env);
+// 	else
+// 	{
+		
+// 	}
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: itaouil <itaouil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 19:45:12 by itaouil           #+#    #+#             */
-/*   Updated: 2022/05/28 21:02:50 by itaouil          ###   ########.fr       */
+/*   Updated: 2022/05/30 14:17:54 by itaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv, char **envp)
 	t_list	*sorted;
 
 	env = create_env(envp);
-	ft_export(&env, NULL);
+	// ft_export(&env, NULL);
 	ft_env(&env);
+	system("leaks a.out");
 }
