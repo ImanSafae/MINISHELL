c72eb63a331bd3ac3bd0ef0cc884a7d3f2403866
Iman Taouil <itaouil@c2r7p6.42nice.fr>
2022-06-06 19:35:12 +0200

    juste repertorie les types de tokens

--- lexer.c
+++ lexer.c
@@ -6,8 +6,23 @@
 /*   By: itaouil <itaouil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 19:45:14 by itaouil           #+#    #+#             */
-/*   Updated: 2022/05/19 19:45:15 by itaouil          ###   ########.fr       */
+/*   Updated: 2022/06/06 19:34:36 by itaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/* Types de TOKENS :
+- commande
+- builtin
+- redirections : > >> < << 
+- infile/outfile
+- caractères spéciaux : $ " ' |
+*/
+
+
+
+void	ft_lexer(int argc, char **argv)
+{
+	if 
+}
